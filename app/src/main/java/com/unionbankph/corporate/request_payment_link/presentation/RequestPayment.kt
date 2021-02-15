435735717f44bb1ec0b38de11ca88c8726b7db9a
Toby Rodin F Roman <trfroman@unionbankph.com>
2021-02-15 09:12:38 +0800

    added Request/Generate Payment Link activity

---
+++ app/src/main/java/com/unionbankph/corporate/request_payment_link/presentation/RequestPayment.kt
@@ -0,0 +1,12 @@
+package com.unionbankph.corporate.request_payment_link.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.unionbankph.corporate.R
+
+class RequestPayment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_request_payment)
+    }
+}
