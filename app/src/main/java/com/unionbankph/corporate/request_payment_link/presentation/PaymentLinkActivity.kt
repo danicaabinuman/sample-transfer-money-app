813e64cdc6b36cbbb9a1ef48bf135b4ef567b77e
Toby Rodin F Roman <trfroman@unionbankph.com>
2021-03-02 09:57:53 +0800

    added storm-10 functionalities + other adjustments to other activities

--- app/src/main/java/com/unionbankph/corporate/request_payment_link/presentation/PaymentLinkActivity.kt
+++ app/src/main/java/com/unionbankph/corporate/request_payment_link/presentation/PaymentLinkActivity.kt
@@ -3,6 +3,7 @@ package com.unionbankph.corporate.request_payment_link.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.unionbankph.corporate.R
+import kotlinx.android.synthetic.main.widget_transparent_dashboard_appbar.*
 
 class PaymentLinkActivity : AppCompatActivity() {
 
@@ -10,6 +11,8 @@ class PaymentLinkActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment_link)
 
+        textViewTitle.text = "Payment Links"
     }
+
 
 }
