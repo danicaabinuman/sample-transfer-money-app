132921c152b0dff7d39d6573d027caf56bbeb541
Toby Rodin F Roman <trfroman@unionbankph.com>
2021-03-15 08:27:08 +0800

    Update on master

---
+++ app/src/main/java/com/unionbankph/corporate/request_payment_link/presentation/setup_payment_link/SetupPaymentLink.kt
@@ -0,0 +1,15 @@
+package com.unionbankph.corporate.request_payment_link.presentation.setup_payment_link
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.unionbankph.corporate.R
+import com.unionbankph.corporate.app.base.BaseActivity
+import com.unionbankph.corporate.app.base.BaseViewModel
+
+class SetupPaymentLink : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setup_payment_links)
+    }
+}
