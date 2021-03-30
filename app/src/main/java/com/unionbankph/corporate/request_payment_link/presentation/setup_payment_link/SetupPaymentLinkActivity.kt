d49154eba525aeebdf3e4f09142610f58a2114b4
Apollo Rafael Tejada <38663021+TejiApple@users.noreply.github.com>
2021-03-30 18:26:36 +0800

    minor update to SetupLinkPaymentActivity

--- app/src/main/java/com/unionbankph/corporate/request_payment_link/presentation/setup_payment_link/SetupPaymentLinkActivity.kt
+++ app/src/main/java/com/unionbankph/corporate/request_payment_link/presentation/setup_payment_link/SetupPaymentLinkActivity.kt
@@ -1,9 +1,11 @@
 package com.unionbankph.corporate.request_payment_link.presentation.setup_payment_link
 
 import android.content.Intent
+import android.text.Html
 import com.unionbankph.corporate.R
 import com.unionbankph.corporate.app.base.BaseActivity
 import com.unionbankph.corporate.request_payment_link.presentation.request_payment.RequestPaymentActivity
+import com.unionbankph.corporate.request_payment_link.presentation.setup_payment_link.terms_of_service.TermsOfServiceActivity
 import kotlinx.android.synthetic.main.activity_setup_payment_links.*
 
 class SetupPaymentLinkActivity : BaseActivity<SetupPaymentLinkViewModel>(R.layout.activity_setup_payment_links) {
@@ -14,6 +16,7 @@ class SetupPaymentLinkActivity : BaseActivity<SetupPaymentLinkViewModel>(R.layou
     }
 
     private fun initViews(){
+        initTermsAndCondition()
 
         btnSetupBusinessLink.setOnClickListener{
             val intent = Intent(this, RequestPaymentActivity::class.java)
@@ -25,5 +28,19 @@ class SetupPaymentLinkActivity : BaseActivity<SetupPaymentLinkViewModel>(R.layou
             finish()
         }
 
+    }
+
+    private fun initTermsAndCondition(){
+        cb_fnc_tnc.text = Html.fromHtml("I have read the " +
+                "<a href=''> Fees and Charges</a>" +
+                " and agree to the " +
+                "<a href=''>Terms and Condition</a>" +
+                " of the Request for Payment")
+        cb_fnc_tnc.isClickable = true
+        cb_fnc_tnc.setOnClickListener{
+            val intent = Intent(this, TermsOfServiceActivity::class.java)
+            startActivity(intent)
+        }
+//        cb_fnc_tnc.movementMethod = LinkMovementMethod.getInstance()
     }
 }
