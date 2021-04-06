631a3bab0a722cf5aba7b4eac9bc59b5014ca9bb
Emil Julius Jimenez <emiljuliusjimenez@gmail.com>
2021-04-06 09:11:31 +0800

    made nominate settlement activity and view model with base activity and base view model

--- app/src/main/java/com/unionbankph/corporate/request_payment_link/presentation/setup_payment_link/nominate_settlement_account/NominateSettlementActivity.kt
+++ app/src/main/java/com/unionbankph/corporate/request_payment_link/presentation/setup_payment_link/nominate_settlement_account/NominateSettlementActivity.kt
@@ -3,50 +3,28 @@ package com.unionbankph.corporate.request_payment_link.presentation.setup_paymen
 import android.accounts.Account
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.unionbankph.corporate.R
-import kotlinx.android.synthetic.main.activity_nominate_settlement.*
+import com.unionbankph.corporate.app.base.BaseActivity
 
-class NominateSettlementActivity : AppCompatActivity() {
+class NominateSettlementActivity : BaseActivity<NominateSettlementViewModel>(R.layout.activity_nominate_settlement) {
 
-    private lateinit var linearLayoutManager: LinearLayoutManager
-
-
+//    private lateinit var linearLayoutManager: LinearLayoutManager
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nominate_settlement)
 
-        linearLayoutManager = LinearLayoutManager(this)
-//        rvNominateAccounts.layoutManager = linearLayoutManager
 
-        include1.findViewById<TextView>(R.id.textViewCorporateName).text = "UPASS CA001 TEST ACCOUNT"
-        include1.findViewById<TextView>(R.id.textViewAccountNumber).text = "0005 9008 0118"
-        include1.findViewById<TextView>(R.id.textViewAccountName).text = "RETAIL REGULAR CHECKING"
-        include1.findViewById<TextView>(R.id.textViewAccountNumber).text = "PHP 338,989,378.27"
-
-        include2.findViewById<TextView>(R.id.textViewCorporateName).text = "ABC SAMPLE ORGANIZATION"
-        include2.findViewById<TextView>(R.id.textViewAccountNumber).text = "0005 9008 0120"
-        include2.findViewById<TextView>(R.id.textViewAccountName).text = "RETAIL REGULAR CHECKING"
-        include2.findViewById<TextView>(R.id.textViewAccountNumber).text = "PHP 21,142,216.21"
-
-        include3.findViewById<TextView>(R.id.textViewCorporateName).text = "UPASS TEST ACCOUNT 5"
-        include3.findViewById<TextView>(R.id.textViewAccountNumber).text = "1021 1002 4433"
-        include3.findViewById<TextView>(R.id.textViewAccountName).text = "RETAIL REG.SAVINGS ACCT"
-        include3.findViewById<TextView>(R.id.textViewAccountNumber).text = "PHP 1,415,166.80"
-
-        include1.setOnClickListener{finish()}
-        include2.setOnClickListener{finish()}
-        include3.setOnClickListener{finish()}
-
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_nominate_settlement)
+//
+//        linearLayoutManager = LinearLayoutManager(this)
+////        rvNominateAccounts.layoutManager = linearLayoutManager
+//
+//    }
 
 
     companion object {
---
+++ app/src/main/java/com/unionbankph/corporate/request_payment_link/presentation/setup_payment_link/nominate_settlement_account/NominateSettlementViewModel.kt
@@ -0,0 +1,10 @@
+package com.unionbankph.corporate.request_payment_link.presentation.setup_payment_link.nominate_settlement_account
+
+import com.unionbankph.corporate.app.base.BaseViewModel
+import javax.inject.Inject
+
+class NominateSettlementViewModel
+@Inject constructor(
+) : BaseViewModel()  {
+
+}
