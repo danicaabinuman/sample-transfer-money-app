b57681841db12a5b2bd264f8c90bea63e6efa390
Apollo Rafael Tejada <38663021+TejiApple@users.noreply.github.com>
2021-04-15 01:04:27 +0800

    added Payment Link Activity Log activity

---
+++ app/src/main/java/com/unionbankph/corporate/payment_link/presentation/payment_link/PaymentLinkActivityLogActivity.kt
@@ -0,0 +1,13 @@
+package com.unionbankph.corporate.payment_link.presentation.payment_link
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.unionbankph.corporate.R
+
+class PaymentLinkActivityLogActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_link_activity_log)
+    }
+}
