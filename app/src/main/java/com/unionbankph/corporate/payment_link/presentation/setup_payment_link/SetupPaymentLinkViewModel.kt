cb36fc3efef2608c518a5e2d178135de9b263e92
Apollo Rafael Tejada <38663021+TejiApple@users.noreply.github.com>
2021-04-23 16:31:42 +0800

    validate merchant use-case and vm implementation

--- app/src/main/java/com/unionbankph/corporate/payment_link/presentation/setup_payment_link/SetupPaymentLinkViewModel.kt
+++ app/src/main/java/com/unionbankph/corporate/payment_link/presentation/setup_payment_link/SetupPaymentLinkViewModel.kt
@@ -2,30 +2,34 @@ package com.unionbankph.corporate.payment_link.presentation.setup_payment_link
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.unionbankph.corporate.account.data.model.Account
 import com.unionbankph.corporate.app.base.BaseViewModel
 import com.unionbankph.corporate.app.common.extension.getDisposableSingleObserver
 import com.unionbankph.corporate.app.common.platform.events.Event
 import com.unionbankph.corporate.common.presentation.viewmodel.state.UiState
 import com.unionbankph.corporate.payment_link.domain.model.form.CreateMerchantForm
 import com.unionbankph.corporate.payment_link.domain.model.response.CreateMerchantResponse
-import com.unionbankph.corporate.payment_link.domain.usecase.GetAccountsUseCase
+import com.unionbankph.corporate.payment_link.domain.model.response.ValidateMerchantByOrganizationResponse
 import com.unionbankph.corporate.payment_link.domain.usecase.CreateMerchantUseCase
+import com.unionbankph.corporate.payment_link.domain.usecase.ValidateMerchantUseCase
 import io.reactivex.rxkotlin.addTo
 import timber.log.Timber
 import javax.inject.Inject
 
 class SetupPaymentLinkViewModel
 @Inject constructor(
-    private val createMerchantUseCase: CreateMerchantUseCase
+    private val createMerchantUseCase: CreateMerchantUseCase,
+    private val validateMerchantUseCase: ValidateMerchantUseCase
 ) : BaseViewModel() {
 
     private val _createMerchantResponse = MutableLiveData<CreateMerchantResponse>()
+    private val _validateMerchantResponse = MutableLiveData<ValidateMerchantByOrganizationResponse>()
 
     val createMerchantResponse: LiveData<CreateMerchantResponse>
         get() =
             _createMerchantResponse
 
+    val validateMerchantResponse: LiveData<ValidateMerchantByOrganizationResponse>
+        get() = _validateMerchantResponse
 
     fun createMerchant(form: CreateMerchantForm){
 
@@ -48,5 +52,22 @@ class SetupPaymentLinkViewModel
         ).addTo(disposables)
     }
 
-
+    fun validateMerchant(){
+        validateMerchantUseCase.execute(
+            getDisposableSingleObserver(
+                {
+                    _validateMerchantResponse.value = it
+                }, {
+                    Timber.e(it, "validateMerchant")
+                    _uiState.value = Event(UiState.Error(it))
+                }
+            ),
+            doOnSubscribeEvent = {
+                _uiState.value = Event(UiState.Loading)
+            },
+            doFinallyEvent = {
+                _uiState.value = Event(UiState.Complete)
+            }
+        ).addTo(disposables)
+    }
 }
