760e5eea80d30f9c625dde96e410aa9d571ef231
CORE\abvelasquez <aries_jayson.velasquez@questronix.com.ph>
2021-06-29 12:56:59 +0800

    initial commit

---
+++ app/src/main/java/com/unionbankph/corporate/payment_link/presentation/payment_link_channels/PaymentLinkChannelsActivity.kt
@@ -0,0 +1,12 @@
+package com.unionbankph.corporate.payment_link.presentation.payment_link_channels
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.unionbankph.corporate.R
+
+class PaymentLinkChannelsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_link_channels)
+    }
+}
