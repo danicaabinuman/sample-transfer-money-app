e6d50d1a51d5cf61203b0bbd5af81cbad5cd7388
CORE\abvelasquez <aries_jayson.velasquez@questronix.com.ph>
2021-07-01 11:22:24 +0800

    Initial Commit: Card acceptance option screen

---
+++ app/src/main/java/com/unionbankph/corporate/payment_link/presentation/setup_payment_link/card_acceptance_option/CardAcceptanceOptionActivity.kt
@@ -0,0 +1,11 @@
+package com.unionbankph.corporate.payment_link.presentation.setup_payment_link.card_acceptance_option
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardAcceptanceOptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_acceptance_option)
+    }
+}
