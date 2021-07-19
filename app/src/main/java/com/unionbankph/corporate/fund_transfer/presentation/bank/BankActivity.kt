54c0932496f43e3e2d9746e9f730290382698b4e
CORE\abvelasquez <aries_jayson.velasquez@questronix.com.ph>
2021-07-19 23:58:33 +0800

    Migrate: BankActivity

--- app/src/main/java/com/unionbankph/corporate/fund_transfer/presentation/bank/BankActivity.kt
+++ app/src/main/java/com/unionbankph/corporate/fund_transfer/presentation/bank/BankActivity.kt
@@ -19,23 +19,22 @@ import com.unionbankph.corporate.app.common.platform.bus.event.base.BaseEvent
 import com.unionbankph.corporate.app.common.platform.events.EventObserver
 import com.unionbankph.corporate.common.data.model.SectionedData
 import com.unionbankph.corporate.common.presentation.viewmodel.state.UiState
+import com.unionbankph.corporate.databinding.ActivityBankBinding
 import com.unionbankph.corporate.fund_transfer.data.model.Bank
 import io.reactivex.rxkotlin.addTo
-import kotlinx.android.synthetic.main.activity_bank.*
-import kotlinx.android.synthetic.main.widget_search_layout.*
-import kotlinx.android.synthetic.main.widget_transparent_appbar.*
 import java.util.concurrent.TimeUnit
 
 class BankActivity :
-    BaseActivity<BankViewModel>(R.layout.activity_bank), BankController.AdapterCallbacks {
+    BaseActivity<ActivityBankBinding, BankViewModel>(),
+    BankController.AdapterCallbacks {
 
     private lateinit var controller: BankController
 
     override fun afterLayout(savedInstanceState: Bundle?) {
         super.afterLayout(savedInstanceState)
-        initToolbar(toolbar, viewToolbar)
+        initToolbar(binding.viewToolbar.toolbar, binding.viewToolbar.appBarLayout)
         setToolbarTitle(
-            tvToolbar,
+            binding.viewToolbar.tvToolbar,
             formatString(
                 if (intent.getStringExtra(EXTRA_CHANNEL_ID) == CHANNEL_CHECK_DEPOSIT) {
                     R.string.title_issuing_bank
@@ -50,27 +49,26 @@ class BankActivity :
     override fun onViewsBound() {
         super.onViewsBound()
         controller = BankController(this)
-        recyclerViewBank.setController(controller)
+        binding.recyclerViewBank.setController(controller)
     }
 
     override fun onViewModelBound() {
         super.onViewModelBound()
-        viewModel = ViewModelProviders.of(this, viewModelFactory)[BankViewModel::class.java]
         viewModel.uiState.observe(this, EventObserver {
             when (it) {
                 is UiState.Loading -> {
-                    if (!swipeRefreshLayoutBanks.isRefreshing) {
-                        viewLoadingState?.visibility = View.VISIBLE
+                    if (!binding.swipeRefreshLayoutBanks.isRefreshing) {
+                        binding.viewLoadingState.root.visibility = View.VISIBLE
                     }
-                    if (viewLoadingState?.visibility == View.VISIBLE) {
-                        swipeRefreshLayoutBanks.isEnabled = true
+                    if (binding.viewLoadingState.root.visibility == View.VISIBLE) {
+                        binding.swipeRefreshLayoutBanks.isEnabled = true
                     }
                 }
                 is UiState.Complete -> {
-                    if (!swipeRefreshLayoutBanks.isRefreshing) {
-                        viewLoadingState?.visibility = View.GONE
-                    } else swipeRefreshLayoutBanks.isRefreshing = false
-                    swipeRefreshLayoutBanks.isEnabled = true
+                    if (!binding.swipeRefreshLayoutBanks.isRefreshing) {
+                        binding.viewLoadingState.root.visibility = View.GONE
+                    } else binding.swipeRefreshLayoutBanks.isRefreshing = false
+                    binding.swipeRefreshLayoutBanks.isEnabled = true
                 }
                 is UiState.Error -> {
                     handleOnError(it.throwable)
@@ -93,15 +91,15 @@ class BankActivity :
     override fun onInitializeListener() {
         super.onInitializeListener()
         initRxSearchEventListener()
-        RxView.clicks(imageViewClearText)
+        RxView.clicks(binding.viewSearchLayout.imageViewClearText)
             .throttleFirst(
                 resources.getInteger(R.integer.time_button_debounce).toLong(),
                 TimeUnit.MILLISECONDS
             )
             .subscribe {
-                editTextSearch.text?.clear()
+                binding.viewSearchLayout.editTextSearch.text?.clear()
             }.addTo(disposables)
-        swipeRefreshLayoutBanks.apply {
+        binding.swipeRefreshLayoutBanks.apply {
             setColorSchemeResources(getAccentColor())
             setOnRefreshListener {
                 fetchBanks()
@@ -125,21 +123,21 @@ class BankActivity :
 
     override fun onClickItem(model: String?) {
         eventBus.inputSyncEvent.emmit(BaseEvent(InputSyncEvent.ACTION_INPUT_BANK_RECEIVER, model!!))
-        if (viewUtil.isSoftKeyboardShown(constraintLayout)) viewUtil.dismissKeyboard(this)
+        if (viewUtil.isSoftKeyboardShown(binding.constraintLayout)) viewUtil.dismissKeyboard(this)
         onBackPressed()
     }
 
     private fun initRxSearchEventListener() {
-        editTextSearch.setOnEditorActionListener(TextView.OnEditorActionListener { v, actionId, event ->
+        binding.viewSearchLayout.editTextSearch.setOnEditorActionListener(TextView.OnEditorActionListener { v, actionId, event ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                viewModel.filterList(editTextSearch.text.toString())
-                editTextSearch.clearFocus()
+                viewModel.filterList(binding.viewSearchLayout.editTextSearch.text.toString())
+                binding.viewSearchLayout.editTextSearch.clearFocus()
                 viewUtil.dismissKeyboard(this@BankActivity)
                 return@OnEditorActionListener true
             }
             false
         })
-        RxTextView.textChangeEvents(editTextSearch)
+        RxTextView.textChangeEvents(binding.viewSearchLayout.editTextSearch)
             .debounce(
                 resources.getInteger(R.integer.time_edit_text_search_debounce).toLong(),
                 TimeUnit.MILLISECONDS
@@ -147,7 +145,7 @@ class BankActivity :
             .subscribeOn(schedulerProvider.computation())
             .observeOn(schedulerProvider.ui())
             .subscribe { filter ->
-                imageViewClearText.visibility(filter.text().isNotEmpty())
+                binding.viewSearchLayout.imageViewClearText.visibility(filter.text().isNotEmpty())
                 if (filter.view().isFocused) {
                     viewModel.filterList(filter.text().toString())
                 }
@@ -156,11 +154,11 @@ class BankActivity :
     }
 
     private fun showEmptyState(data: MutableList<SectionedData<Bank>>) {
-        textViewState.text = getString(R.string.title_no_banks)
+        binding.textViewState.text = getString(R.string.title_no_banks)
         if (data.size > 0) {
-            if (textViewState.visibility == View.VISIBLE) textViewState.visibility = View.GONE
+            if (binding.textViewState.visibility == View.VISIBLE) binding.textViewState.visibility = View.GONE
         } else {
-            textViewState.visibility = View.VISIBLE
+            binding.textViewState.visibility = View.VISIBLE
         }
     }
 
@@ -168,4 +166,10 @@ class BankActivity :
         const val EXTRA_CHANNEL_ID = "channel_id"
         const val CHANNEL_CHECK_DEPOSIT = "5"
     }
+
+    override val layoutId: Int
+        get() = R.layout.activity_bank
+
+    override val viewModelClassType: Class<BankViewModel>
+        get() = BankViewModel::class.java
 }
