ef2a37550fa7bae4bb28781a1351eb2410845e08
CORE\abvelasquez <aries_jayson.velasquez@questronix.com.ph>
2021-07-20 18:14:54 +0800

    Migrate: SwiftConfirmationActivity

--- app/src/main/java/com/unionbankph/corporate/fund_transfer/presentation/swift/SwiftConfirmationActivity.kt
+++ app/src/main/java/com/unionbankph/corporate/fund_transfer/presentation/swift/SwiftConfirmationActivity.kt
@@ -33,15 +33,14 @@ import com.unionbankph.corporate.common.presentation.viewmodel.ShowGeneralGetOrg
 import com.unionbankph.corporate.common.presentation.viewmodel.ShowTutorialError
 import com.unionbankph.corporate.common.presentation.viewmodel.ShowTutorialHasTutorial
 import com.unionbankph.corporate.common.presentation.viewmodel.TutorialViewModel
+import com.unionbankph.corporate.databinding.ActivityFundTransferConfirmationSwiftBinding
 import com.unionbankph.corporate.fund_transfer.data.form.FundTransferSwiftForm
 import com.unionbankph.corporate.fund_transfer.presentation.organization_transfer.OrganizationTransferActivity
 import io.reactivex.rxkotlin.addTo
-import kotlinx.android.synthetic.main.activity_fund_transfer_confirmation_swift.*
-import kotlinx.android.synthetic.main.widget_transparent_org_appbar.*
 import java.util.concurrent.TimeUnit
 
 class SwiftConfirmationActivity :
-    BaseActivity<SwiftViewModel>(R.layout.activity_fund_transfer_confirmation_swift),
+    BaseActivity<ActivityFundTransferConfirmationSwiftBinding, SwiftViewModel>(),
     OnTutorialListener {
 
     private var fundTransferSwiftForm: FundTransferSwiftForm? = null
@@ -50,7 +49,7 @@ class SwiftConfirmationActivity :
 
     override fun afterLayout(savedInstanceState: Bundle?) {
         super.afterLayout(savedInstanceState)
-        initToolbar(toolbar, viewToolbar)
+        initToolbar(binding.viewToolbar.toolbar, binding.viewToolbar.appBarLayout)
         setDrawableBackButton(R.drawable.ic_close_white_24dp)
     }
 
@@ -74,7 +73,7 @@ class SwiftConfirmationActivity :
 
     override fun onInitializeListener() {
         super.onInitializeListener()
-        RxView.clicks(buttonEdit)
+        RxView.clicks(binding.buttonEdit)
             .throttleFirst(
                 resources.getInteger(R.integer.time_button_debounce).toLong(),
                 TimeUnit.MILLISECONDS
@@ -84,7 +83,7 @@ class SwiftConfirmationActivity :
             }
             .addTo(disposables)
 
-        RxView.clicks(buttonSubmit)
+        RxView.clicks(binding.buttonSubmit)
             .throttleFirst(
                 resources.getInteger(R.integer.time_button_debounce).toLong(),
                 TimeUnit.MILLISECONDS
@@ -135,15 +134,15 @@ class SwiftConfirmationActivity :
 
     override fun onEndedTutorial(view: View?, viewTarget: View) {
         if (isSkipTutorial) {
-            scrollView.post { scrollView.smoothScrollTo(0, 0) }
+            binding.scrollView.post { binding.scrollView.smoothScrollTo(0, 0) }
         } else {
             val radius = resources.getDimension(R.dimen.button_radius)
             when (view) {
-                buttonEdit -> {
-                    viewUtil.setFocusOnView(scrollView, buttonSubmit)
+                binding.buttonEdit -> {
+                    viewUtil.setFocusOnView(binding.scrollView, binding.buttonSubmit)
                     tutorialEngineUtil.startTutorial(
                         this,
-                        buttonSubmit,
+                        binding.buttonSubmit,
                         R.layout.frame_tutorial_lower_right,
                         radius,
                         false,
@@ -153,7 +152,7 @@ class SwiftConfirmationActivity :
                     )
                 }
                 else -> {
-                    scrollView.post { scrollView.smoothScrollTo(0, 0) }
+                    binding.scrollView.post { binding.scrollView.smoothScrollTo(0, 0) }
                     // tutorialViewModel.setTutorial(TutorialScreenEnum.SWIFT_CONFIRMATION, false)
                 }
             }
@@ -185,8 +184,8 @@ class SwiftConfirmationActivity :
             when (it) {
                 is ShowGeneralGetOrganizationName -> {
                     setToolbarTitle(
-                        textViewTitle,
-                        textViewCorporationName,
+                        binding.viewToolbar.textViewTitle,
+                        binding.viewToolbar.textViewCorporationName,
                         formatString(R.string.title_transfer_confirmation),
                         it.orgName
                     )
@@ -197,7 +196,6 @@ class SwiftConfirmationActivity :
     }
 
     private fun initViewModel() {
-        viewModel = ViewModelProviders.of(this, viewModelFactory)[SwiftViewModel::class.java]
         viewModel.state.observe(this, Observer {
             when (it) {
                 is ShowSwiftLoading -> {
@@ -330,7 +328,7 @@ class SwiftConfirmationActivity :
         fundTransferSwiftForm: FundTransferSwiftForm,
         account: Account
     ) {
-        textViewTransferFrom.text =
+        binding.textViewTransferFrom.text =
             Html.fromHtml(
                 String.format(
                     getString(R.string.params_account_detail),
@@ -340,7 +338,7 @@ class SwiftConfirmationActivity :
                 )
             )
 
-        textViewTransferTo.text = if (fundTransferSwiftForm.beneficiaryMasterForm == null) {
+        binding.textViewTransferTo.text = if (fundTransferSwiftForm.beneficiaryMasterForm == null) {
             Html.fromHtml(
                 String.format(
                     getString(R.string.params_account_detail),
@@ -363,7 +361,7 @@ class SwiftConfirmationActivity :
             )
         }
 
-        textViewReceivingBank.text = if (fundTransferSwiftForm.beneficiaryMasterForm == null) {
+        binding.textViewReceivingBank.text = if (fundTransferSwiftForm.beneficiaryMasterForm == null) {
             Html.fromHtml(
                 String.format(
                     getString(R.string.params_account_detail),
@@ -383,7 +381,7 @@ class SwiftConfirmationActivity :
             )
         }
 
-        textViewProposedTransferDate.text =
+        binding.textViewProposedTransferDate.text =
             if (fundTransferSwiftForm.immediate!!)
                 getString(R.string.title_immediately)
             else
@@ -392,7 +390,7 @@ class SwiftConfirmationActivity :
                     ViewUtil.DATE_FORMAT_ISO,
                     ViewUtil.DATE_FORMAT_DEFAULT
                 )
-        textViewAmount.text =
+        binding.textViewAmount.text =
             AutoFormatUtil().formatWithTwoDecimalPlaces(
                 fundTransferSwiftForm.amount.toString(),
                 account.currency
@@ -404,9 +402,9 @@ class SwiftConfirmationActivity :
                 intent.getStringExtra(EXTRA_CUSTOM_SERVICE_FEE)
             )
             if (0.00 >= customServiceFee.value?.toDouble() ?: 0.00) {
-                textViewServiceFee.text = getString(R.string.value_service_fee_free)
+                binding.textViewServiceFee.text = getString(R.string.value_service_fee_free)
             } else {
-                textViewServiceFee.text = formatString(
+                binding.textViewServiceFee.text = formatString(
                     R.string.value_service,
                     autoFormatUtil.formatWithTwoDecimalPlaces(
                         customServiceFee.value,
@@ -414,18 +412,18 @@ class SwiftConfirmationActivity :
                     )
                 )
             }
-            textViewServiceDiscountFee.visibility(true)
-            viewBorderServiceDiscountFee.visibility(true)
+            binding.textViewServiceDiscountFee.visibility(true)
+            binding.viewBorderServiceDiscountFee.visibility(true)
         } else {
-            textViewServiceDiscountFee.visibility(false)
-            viewBorderServiceDiscountFee.visibility(false)
+            binding.textViewServiceDiscountFee.visibility(false)
+            binding.viewBorderServiceDiscountFee.visibility(false)
         }
         if (intent.getStringExtra(EXTRA_SERVICE_FEE) != null) {
             val serviceFee = JsonHelper.fromJson<ServiceFee>(
                 intent.getStringExtra(EXTRA_SERVICE_FEE)
             )
             if (intent.getStringExtra(EXTRA_CUSTOM_SERVICE_FEE) != null) {
-                textViewServiceDiscountFee.text = formatString(
+                binding.textViewServiceDiscountFee.text = formatString(
                     R.string.value_service,
                     autoFormatUtil.formatWithTwoDecimalPlaces(
                         serviceFee.value,
@@ -433,7 +431,7 @@ class SwiftConfirmationActivity :
                     )
                 )
             } else {
-                textViewServiceFee.text = formatString(
+                binding.textViewServiceFee.text = formatString(
                     R.string.value_service,
                     autoFormatUtil.formatWithTwoDecimalPlaces(
                         serviceFee.value,
@@ -442,12 +440,12 @@ class SwiftConfirmationActivity :
                 )
             }
         } else {
-            textViewServiceFee.text = getString(R.string.value_service_fee_free)
+            binding.textViewServiceFee.text = getString(R.string.value_service_fee_free)
         }
 
-        textViewChannel.text = intent.getStringExtra(EXTRA_ACCOUNT_TYPE)
-        textViewPurpose.text = fundTransferSwiftForm.purposeDesc
-        textViewRemarks.text = viewUtil.getStringOrEmpty(fundTransferSwiftForm.remarks)
+        binding.textViewChannel.text = intent.getStringExtra(EXTRA_ACCOUNT_TYPE)
+        binding.textViewPurpose.text = fundTransferSwiftForm.purposeDesc
+        binding.textViewRemarks.text = viewUtil.getStringOrEmpty(fundTransferSwiftForm.remarks)
 
         val reminders = JsonHelper.fromListJson<String>(intent.getStringExtra(EXTRA_REMINDERS))
         if (reminders.isNotEmpty()) {
@@ -455,27 +453,27 @@ class SwiftConfirmationActivity :
             reminders.forEach {
                 remindersContent.append("$it\n\n")
             }
-            tv_reminders.text = remindersContent
+            binding.tvReminders.text = remindersContent
         } else {
-            border_reminders.isInvisible = true
-            tv_reminders_title.isInvisible = true
-            tv_reminders.isVisible = false
+            binding.borderReminders.isInvisible = true
+            binding.tvRemindersTitle.isInvisible = true
+            binding.tvReminders.isVisible = false
         }
         if (fundTransferSwiftForm.immediate!!) {
-            textViewStartDateTitle.visibility = View.GONE
-            textViewStartDate.visibility = View.GONE
-            view5.visibility = View.GONE
-            textViewFrequencyTitle.visibility = View.GONE
-            textViewFrequency.visibility = View.GONE
-            view6.visibility = View.GONE
-            textViewEndDateTitle.visibility = View.GONE
-            textViewEndDate.visibility = View.GONE
-            view7.visibility = View.GONE
-            textViewProposedTransferDateTitle.visibility = View.VISIBLE
-            textViewProposedTransferDate.visibility = View.VISIBLE
-            view4.visibility = View.VISIBLE
+            binding.textViewStartDateTitle.visibility = View.GONE
+            binding.textViewStartDate.visibility = View.GONE
+            binding.view5.visibility = View.GONE
+            binding.textViewFrequencyTitle.visibility = View.GONE
+            binding.textViewFrequency.visibility = View.GONE
+            binding.view6.visibility = View.GONE
+            binding.textViewEndDateTitle.visibility = View.GONE
+            binding.textViewEndDate.visibility = View.GONE
+            binding.view7.visibility = View.GONE
+            binding.textViewProposedTransferDateTitle.visibility = View.VISIBLE
+            binding.textViewProposedTransferDate.visibility = View.VISIBLE
+            binding.view4.visibility = View.VISIBLE
         } else {
-            textViewStartDate.text =
+            binding.textViewStartDate.text =
                 viewUtil.getDateFormatByDateString(
                     fundTransferSwiftForm.transferDate,
                     ViewUtil.DATE_FORMAT_ISO,
@@ -487,36 +485,36 @@ class SwiftConfirmationActivity :
                     ViewUtil.DATE_FORMAT_ISO,
                     ViewUtil.DATE_FORMAT_DATE
                 )
-                textViewEndDate.text =
+                binding.textViewEndDate.text =
                     ("${fundTransferSwiftForm.occurrencesText}\n(Until $endDate)")
             } else {
-                textViewEndDate.text = fundTransferSwiftForm.occurrencesText
+                binding.textViewEndDate.text = fundTransferSwiftForm.occurrencesText
             }
-            textViewFrequency.text = fundTransferSwiftForm.frequency
+            binding.textViewFrequency.text = fundTransferSwiftForm.frequency
             if (fundTransferSwiftForm.frequency == getString(R.string.title_one_time)) {
-                textViewEndDate.visibility = View.GONE
-                textViewEndDateTitle.visibility = View.GONE
-                view7.visibility = View.GONE
-                textViewFrequencyTitle.visibility = View.GONE
-                textViewFrequency.visibility = View.GONE
-                view6.visibility = View.GONE
-                textViewStartDateTitle.visibility = View.GONE
-                textViewStartDate.visibility = View.GONE
-                view5.visibility = View.GONE
+                binding.textViewEndDate.visibility = View.GONE
+                binding.textViewEndDateTitle.visibility = View.GONE
+                binding.view7.visibility = View.GONE
+                binding.textViewFrequencyTitle.visibility = View.GONE
+                binding.textViewFrequency.visibility = View.GONE
+                binding.view6.visibility = View.GONE
+                binding.textViewStartDateTitle.visibility = View.GONE
+                binding.textViewStartDate.visibility = View.GONE
+                binding.view5.visibility = View.GONE
             } else {
-                textViewProposedTransferDateTitle.visibility = View.GONE
-                textViewProposedTransferDate.visibility = View.GONE
-                view4.visibility = View.GONE
+                binding.textViewProposedTransferDateTitle.visibility = View.GONE
+                binding.textViewProposedTransferDate.visibility = View.GONE
+                binding.view4.visibility = View.GONE
             }
         }
     }
 
     private fun startViewTutorial() {
-        viewUtil.setFocusOnView(scrollView, buttonEdit)
+        viewUtil.setFocusOnView(binding.scrollView, binding.buttonEdit)
         val radius = resources.getDimension(R.dimen.button_radius)
         tutorialEngineUtil.startTutorial(
             this,
-            buttonEdit,
+            binding.buttonEdit,
             R.layout.frame_tutorial_lower_left,
             radius,
             false,
@@ -534,4 +532,10 @@ class SwiftConfirmationActivity :
         const val EXTRA_CUSTOM_SERVICE_FEE = "custom_service_fee"
         const val EXTRA_REMINDERS = "reminders"
     }
+
+    override val layoutId: Int
+        get() = R.layout.activity_fund_transfer_confirmation_swift
+
+    override val viewModelClassType: Class<SwiftViewModel>
+        get() = SwiftViewModel::class.java
 }
