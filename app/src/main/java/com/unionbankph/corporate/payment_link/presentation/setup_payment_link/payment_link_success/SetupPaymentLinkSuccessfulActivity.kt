a31cfec83bbce568b87d7dd2cad2575c7a5b362e
CORE\abvelasquez <aries_jayson.velasquez@questronix.com.ph>
2021-07-21 13:31:30 +0800

    Migrate: SetupPaymentLinkSuccessfulActivity

--- app/src/main/java/com/unionbankph/corporate/payment_link/presentation/setup_payment_link/payment_link_success/SetupPaymentLinkSuccessfulActivity.kt
+++ app/src/main/java/com/unionbankph/corporate/payment_link/presentation/setup_payment_link/payment_link_success/SetupPaymentLinkSuccessfulActivity.kt
@@ -7,20 +7,18 @@ import com.unionbankph.corporate.app.base.BaseActivity
 import com.unionbankph.corporate.app.common.platform.bus.event.TransactSyncEvent
 import com.unionbankph.corporate.app.common.platform.bus.event.base.BaseEvent
 import com.unionbankph.corporate.app.dashboard.DashboardViewModel
+import com.unionbankph.corporate.databinding.ActivitySetupPaymentLinkSuccessBinding
 import com.unionbankph.corporate.payment_link.presentation.onboarding.RequestPaymentSplashActivity
 import com.unionbankph.corporate.payment_link.presentation.request_payment.RequestForPaymentActivity
-import kotlinx.android.synthetic.main.activity_setup_payment_link_success.*
 
-class SetupPaymentLinkSuccessfulActivity : BaseActivity<SetupPaymentLinkSuccessfulViewModel>(R.layout.activity_setup_payment_link_success) {
+class SetupPaymentLinkSuccessfulActivity :
+    BaseActivity<ActivitySetupPaymentLinkSuccessBinding, SetupPaymentLinkSuccessfulViewModel>() {
 
     private var fromWhatTab : String? = null
 
     override fun onViewModelBound() {
         super.onViewModelBound()
-        viewModel = ViewModelProviders.of(
-            this,
-            viewModelFactory
-        )[SetupPaymentLinkSuccessfulViewModel::class.java]
+
     }
 
     override fun onViewsBound() {
@@ -34,7 +32,7 @@ class SetupPaymentLinkSuccessfulActivity : BaseActivity<SetupPaymentLinkSuccessf
             fromWhatTab = DashboardViewModel.FROM_REQUEST_PAYMENT_BUTTON
         }
 
-        btnTakeMeThere.setOnClickListener{
+        binding.btnTakeMeThere.setOnClickListener{
 
             when(fromWhatTab){
                 DashboardViewModel.FROM_REQUEST_PAYMENT_BUTTON -> {
@@ -51,5 +49,11 @@ class SetupPaymentLinkSuccessfulActivity : BaseActivity<SetupPaymentLinkSuccessf
             }
         }
     }
+
+    override val layoutId: Int
+        get() = R.layout.activity_setup_payment_link_success
+
+    override val viewModelClassType: Class<SetupPaymentLinkSuccessfulViewModel>
+        get() = SetupPaymentLinkSuccessfulViewModel::class.java
 }
 
