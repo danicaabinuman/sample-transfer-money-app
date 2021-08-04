417096ba87ade27acabd8d3536cb345da026d4ec
Apollo Rafael Tejada <zxctejs@gmail.com>
2021-08-04 11:14:52 +0800

    added function for pdf selection

--- app/src/main/java/com/unionbankph/corporate/payment_link/presentation/setup_payment_link/card_acceptance_option/upload_documents/CardAcceptanceUploadDocumentsActivity.kt
+++ app/src/main/java/com/unionbankph/corporate/payment_link/presentation/setup_payment_link/card_acceptance_option/upload_documents/CardAcceptanceUploadDocumentsActivity.kt
@@ -1,31 +1,38 @@
 package com.unionbankph.corporate.payment_link.presentation.setup_payment_link.card_acceptance_option.upload_documents
 
-import android.os.Bundle
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.pdf.PdfRenderer
+import android.net.Uri
+import android.os.*
 import android.view.MenuItem
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
 import com.unionbankph.corporate.R
 import com.unionbankph.corporate.app.base.BaseActivity
-import com.unionbankph.corporate.payment_link.presentation.onboarding.OnboardingUploadPhotosFragment
-import com.unionbankph.corporate.payment_link.presentation.setup_payment_link.card_acceptance_option.YesAcceptCardPaymentsViewModel
-import kotlinx.android.synthetic.main.activity_onboarding_upload_photos.*
 import kotlinx.android.synthetic.main.activity_onboarding_upload_photos.viewToolbar
 import kotlinx.android.synthetic.main.activity_upload_documents.*
-import kotlinx.android.synthetic.main.bottom_sheet_upload_photos.*
-import kotlinx.android.synthetic.main.widget_transparent_org_appbar.*
-import kotlinx.android.synthetic.main.widget_transparent_org_appbar.textViewTitle
-import kotlinx.android.synthetic.main.widget_transparent_org_appbar.toolbar
 import kotlinx.android.synthetic.main.widget_transparent_rmo_appbar.*
+import java.io.File
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class CardAcceptanceUploadDocumentsActivity :
-    BaseActivity<CardAcceptanceUploadDocumentsViewModel>(R.layout.activity_upload_documents) {
+    BaseActivity<CardAcceptanceUploadDocumentsViewModel>(R.layout.activity_upload_documents),
+    CardAcceptanceUploadDocumentFragment.OnUploadBIRDocs {
 
-    private var onboardingUploadFragment: OnboardingUploadPhotosFragment? = null
+    private var uploadBIRFragment: CardAcceptanceUploadDocumentFragment? = null
+    private var fileUri: Uri? = null
+    lateinit var imgView: ImageView
+    val REQUEST_CODE = 200
 
     override fun afterLayout(savedInstanceState: Bundle?) {
         super.afterLayout(savedInstanceState)
         initToolbar(toolbar, viewToolbar)
         setDrawableBackButton(
-            R.drawable.ic_close_orange,
+            R.drawable.ic_msme_back_button_orange,
             R.color.colorSMEMediumOrange,
             true
         )
@@ -44,7 +51,7 @@ class CardAcceptanceUploadDocumentsActivity :
     override fun onViewsBound() {
         super.onViewsBound()
 
-        textViewTitle.text = "Upload Document"
+        btnSaveAndExit.visibility = View.GONE
         btnUploadBIRDocs.setOnClickListener {
             showbottomSheetDialog()
         }
@@ -52,16 +59,71 @@ class CardAcceptanceUploadDocumentsActivity :
 
     private fun showbottomSheetDialog() {
 
-        if (onboardingUploadFragment == null) {
-            onboardingUploadFragment = OnboardingUploadPhotosFragment.newInstance()
+        if (uploadBIRFragment == null) {
+            uploadBIRFragment = CardAcceptanceUploadDocumentFragment.newInstance()
         }
 
-        onboardingUploadFragment!!.show(supportFragmentManager, OnboardingUploadPhotosFragment.TAG)
-        if (onboardingUploadFragment!!.isVisible){
-            clUploadPhotoOption.visibility = View.GONE
-            clUploadBIROption.visibility = View.VISIBLE
+        uploadBIRFragment!!.show(supportFragmentManager, CardAcceptanceUploadDocumentFragment.TAG)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        imgView = findViewById(R.id.ivPreviewBIR)
+        when (requestCode) {
+            REQUEST_CODE ->
+                if (resultCode == RESULT_OK) {
+                    if (data?.data != null) {
+//                        clUploadBIRDocs.visibility = View.GONE
+//                        clPreviewBIR.visibility = View.VISIBLE
+
+                        val fileUri = data.data!!.toString()
+                        val file = File(fileUri)
+                        clUploadBIRDocs.visibility = View.VISIBLE
+                        val snackbarView = findViewById<TextView>(R.id.snackbar)
+                        val snackUploading = Snackbar.make(snackbarView, "Uploading photo...", Snackbar.LENGTH_LONG).setAnchorView(R.id.btnNext)
+                        snackUploading.show()
+
+                        val handler = Handler(Looper.getMainLooper())
+                        handler.postDelayed({
+                            btnNext.visibility = View.VISIBLE
+                        }, 3000)
+                    }
+//                        val fileDescriptor = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY)
+//                        val pdfRenderer = PdfRenderer(fileDescriptor)
+//                        val rendererPage = pdfRenderer.openPage(0)
+//                        val rendererPageWidth = rendererPage.width
+//                        val rendererPageHeight = rendererPage.height
+//                        val bitmap = Bitmap.createBitmap(rendererPageWidth, rendererPageHeight, Bitmap.Config.ARGB_8888)
+//                        rendererPage.render(bitmap,null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+//                        imgView.setImageBitmap(bitmap)
+                }
+        }
+    }
+
+    private fun openFiles() {
+        if (Build.VERSION.SDK_INT < 19) {
+            val intent = Intent()
+            intent.type = "application/pdf"
+            intent.action = Intent.ACTION_GET_CONTENT
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            startActivityForResult(
+                Intent.createChooser(intent, "Choose file"),
+                REQUEST_CODE
+            )
+        } else {
+            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            intent.addCategory(Intent.CATEGORY_OPENABLE)
+            intent.type = "application/pdf"
+            startActivityForResult(intent, REQUEST_CODE)
         }
 
     }
 
+    override fun openFileManager() {
+        openFiles()
+        uploadBIRFragment?.dismiss()
+    }
 }
+
+
+
